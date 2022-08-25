5cb202fd20620a2ad06b7552edff80481cd983e2
Immanuel <immanuelgk@gmail.com>
2022-08-25 11:34:11 +0530

    added gui

---
+++ TicTac-Toe-Game/src/main/java/com/aurionpro/test/TicTacToeUITest.java
@@ -0,0 +1,9 @@
+package com.aurionpro.test;
+
+public class TicTacToeUITest {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		TicTacToeUserNameUI ticObj=new TicTacToeUserNameUI();
+	}
+}
